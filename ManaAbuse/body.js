0515ca197ce67c67e0346fc692fca9bfce0b2ff4
Exieros <Exieros@gmail.com>
2016-07-18 08:14:27 +1100

    D2JS
    new script ManaAbuse added

---
+++ ManaAbuse/body.js
@@ -0,0 +1,46 @@
+﻿var ping = 0.25
+
+var AbuseManaItems = [
+	"item_arcane_boots",
+	"item_guardian_greaves",
+	"item_soul_ring"
+]
+
+Game.Functions.ManaAbuseF = function(){
+	var MyEnt = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID())
+	var XYZ = Entities.GetAbsOrigin( MyEnt )
+	var uix = Game.WorldToScreenX( XYZ[0], XYZ[1], (XYZ[2]) )
+	var uiy = Game.WorldToScreenY( XYZ[0], XYZ[1], (XYZ[2]) )
+	var Inv = Game.GetInventory(MyEnt)
+	var DropItems = []
+	for(key in Inv){
+		var Item = parseInt(Inv[key])
+		var ItemName = Abilities.GetAbilityName( Item )
+		var ManaPool = 0
+		ManaPool += Abilities.GetSpecialValueFor( Item, 'bonus_intellect' )
+		ManaPool += Abilities.GetSpecialValueFor( Item, 'bonus_all_stats' )
+		ManaPool += Abilities.GetSpecialValueFor( Item, 'bonus_mana' )
+		if(ManaPool>0&&AbuseManaItems.indexOf(ItemName)==-1){
+			Game.DropItem(MyEnt,Item,XYZ,false)
+			DropItems.push(Item)
+		}
+	}
+	for(key in Inv){
+		var Item = parseInt(Inv[key])
+		var ItemName = Abilities.GetAbilityName( Item )
+		if(AbuseManaItems.indexOf(ItemName)!=-1)
+			Game.CastNoTarget(MyEnt,Item,false)
+	}
+	$.Schedule(ping,function(){
+		var EntsOnCursor = GameUI.FindScreenEntities( [uix,uiy] )
+		for(key in EntsOnCursor){
+			var Item = parseInt(EntsOnCursor[key].entityIndex)
+			Game.PuckupItem(MyEnt,Item,true)
+		}
+	})
+	
+}
+if(!Game.ManaAbuseCreate){
+	Game.ManaAbuseCreate = true
+	Game.AddCommand("__ManaAbuse", Game.Functions.ManaAbuseF, "", 0)
+}
